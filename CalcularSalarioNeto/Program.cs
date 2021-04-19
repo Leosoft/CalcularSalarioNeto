473e3e18d2413ed7a712d78e65f8f123e9312838
Leosoft <leosoft3031@gmail.com>
2021-04-19 13:25:52 -0300

    Agregar archivos de proyecto.

---
+++ CalcularSalarioNeto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CalcularSalarioNeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double descuento, salarioBruto,neto, bono;
+             
+            Console.Write("Ingresa el salario bruto: ");
+            salarioBruto = float.Parse(Console.ReadLine());
+
+
+            if(salarioBruto >= 2000)
+            {
+                descuento = salarioBruto * 0.13;
+                bono = 0.13;
+                neto = salarioBruto - descuento + bono;
+            }
+            else
+            {
+                descuento = salarioBruto * 0.10;
+                bono = 0.10;
+                neto = salarioBruto - descuento + bono;
+            }
+
+            //ToString("N2") muestra solo 2 numeros decimales luego del calculo del neto.
+            Console.WriteLine("el salario neto es: " + "$" + neto.ToString("N2"));
+            
+        }
+    }
+}
